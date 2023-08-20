63efcc28541c330592c1eaf8d80691418c2703f7
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2023-08-20 10:48:07 +0530

    Add basic BF structure

--- bfinterpreter.ml
+++ bfinterpreter.ml
@@ -1,1 +1,11 @@
-type t = unit
+type t =
+  { tape : bytes;
+    length : int;
+    current_position : int
+  }
+
+let create (length : int) =
+  { tape = Bytes.make length (char_of_int 0);
+    length = length;
+    current_position = 0;
+  }
