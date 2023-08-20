222f0680507b33b3f939e5a1a894b06b7806f728
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2023-08-20 19:09:06 +0530

    Move to Alcotest

--- bftest.ml
+++ bftest.ml
@@ -1,6 +1,21 @@
 open Bfinterpreter
 
-let _ =
-  let bfinterp = create 1000 "++++++++  [>++++   [>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++   .>>.<-.<.+++.------.--------.>>+.>++."
+let test_zero () =
+  let bf = create 1000 "++++++++++++++++++++++++++++++++++++++++++++++++" in
+  interpret bf;
+  Alcotest.(check string) "same tape" "0" (String.sub (Bfinterpreter.tape_as_string bf) 0 1)
+
+let test_helloworld () =
+  let bf = create 1000 "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++ .>>.<-.<.+++.------.--------.>>+.>++."
   in
-  interpret bfinterp
+  interpret bf;
+  let template_string = "HdW!" in
+  Alcotest.(check string) "same tape" template_string (String.sub (Bfinterpreter.tape_as_string bf) 2 (String.length template_string))
+
+
+let () =
+  let open Alcotest in
+  run "Basic BF" [
+      "increments", [ test_case "Test increments" `Slow test_zero ];
+      "helloworld", [ test_case "Hello World!" `Slow test_helloworld ];
+    ]
