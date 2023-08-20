e7fd268095dbe4da66499b82262c1aa4592839b8
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2023-08-20 10:54:16 +0530

    Add increment and decrement functions

--- bfinterpreter.ml
+++ bfinterpreter.ml
@@ -9,3 +9,17 @@ let create (length : int) =
     length = length;
     current_position = 0;
   }
+
+let increment_data_pointer bfstruct =
+  { bfstruct with current_position = bfstruct.current_position + 1 }
+
+let decrement_data_pointer bfstruct =
+  { bfstruct with current_position = bfstruct.current_position - 1 }
+
+let increment_value_at_pointer bfstruct =
+  let current_value = Bytes.get bfstruct.tape bfstruct.current_position |> int_of_char in
+  Bytes.set bfstruct.tape bfstruct.current_position (char_of_int (current_value + 1))
+
+let decrement_value_at_pointer bfstruct =
+  let current_value = Bytes.get bfstruct.tape bfstruct.current_position |> int_of_char in
+  Bytes.set bfstruct.tape bfstruct.current_position (char_of_int (current_value - 1))
