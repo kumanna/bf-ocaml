068a82abd481eeb875d25cfffd1eb3afffbbc56c
Kumar Appaiah <a.kumar@alumni.iitm.ac.in>
2023-08-20 19:10:43 +0530

    Add test

--- bftest.ml
+++ bftest.ml
@@ -5,6 +5,11 @@ let test_zero () =
   interpret bf;
   Alcotest.(check string) "same tape" "0" (String.sub (Bfinterpreter.tape_as_string bf) 0 1)
 
+let test_pointermove () =
+  let bf = create 1000 ">>++++++++++++++++++++++++++++++++++++++++++++++++" in
+  interpret bf;
+  Alcotest.(check string) "same tape" "0" (String.sub (Bfinterpreter.tape_as_string bf) 2 1)
+
 let test_helloworld () =
   let bf = create 1000 "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++ .>>.<-.<.+++.------.--------.>>+.>++."
   in
@@ -17,5 +22,6 @@ let () =
   let open Alcotest in
   run "Basic BF" [
       "increments", [ test_case "Test increments" `Slow test_zero ];
+      "pointermove", [ test_case "Test pointer move" `Slow test_pointermove ];
       "helloworld", [ test_case "Hello World!" `Slow test_helloworld ];
     ]
